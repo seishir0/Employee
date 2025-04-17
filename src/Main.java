8671c9840305f94dfc67e062fd25f5fb63d5fa63
seishir0 <quizwxx@gmail.com>
2025-04-17 12:35:11 +0300

    Доработка файла

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Employee emp = new Employee("Іван Петренко", "Менеджер", "ivan@email.com", "+380501234567", 35);
-        Car car = new Car();
-        car.start();
+        Car Car = new Car();
+        Car.start();
     }
 }
